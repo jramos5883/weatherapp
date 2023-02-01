5c0a336c0338df879049a30a6aef1577d84f29c0
John Ramos <jramos5883@gmail.com>
2023-02-01 15:03:02 -0800

    Finished functionallity of weather app

--- app.js
+++ app.js
@@ -29,12 +29,16 @@ app.post("/", function (req, res) {
       const temp = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
+      const humidity = weatherData.main.humidity;
+      const windspd = weatherData.wind.speed;
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-      res.write("<p>The weather is currently " + weatherDescription + "</p>");
-      res.write(
-        "<h1>The temperature in " + query +  " is " + temp + " degrees F.</h1>"
-      );
+      res.write("<h1>The weather in " + query + " is currently " + weatherDescription + "</h1>");
       res.write("<img src=" + imageURL + ">");
+      res.write(
+        "<h2>The temperature is " + temp + " degrees F.</h2>"
+      );
+      res.write("<h2>The humidity is " + humidity + "%.</h2>")
+      res.write("<h2>The wind speed is " + windspd + "mph.</h2>")
       res.send();
     });
   });
