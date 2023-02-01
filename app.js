cbac30603ef52a5c8f9eee906228abbf01737073
John Ramos <jramos5883@gmail.com>
2023-02-01 12:29:43 -0800

    Great review of express and working with APIs, need to review functionallity and finishe styling.

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function (req, res) {
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=London&appid=9b55d4af647732f3bd6ef206211780bf&units=imperial";
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const query = req.body.cityName;
+  const apiKey = "9b55d4af647732f3bd6ef206211780bf";
+  const units = "imperial";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    units;
   https.get(url, function (response) {
     console.log(response.statusCode);
 
     response.on("data", function (data) {
-      const weatherData = JSON.parse(data)
+      const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
-      console.log(temp);
-      console.log(weatherDescription);
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write("<p>The weather is currently " + weatherDescription + "</p>");
+      res.write(
+        "<h1>The temperature in " + query +  " is " + temp + " degrees F.</h1>"
+      );
+      res.write("<img src=" + imageURL + ">");
+      res.send();
     });
   });
-
-  res.send("Server is up and running.");
 });
 
 app.listen(3000, function () {
